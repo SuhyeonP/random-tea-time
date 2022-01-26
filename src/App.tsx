4d714c3810f57c5f63ebcd21bdda26b827e911a8
suhyeonP <ahah12k@gmail.com>
2022-01-26 13:25:43 +0900

    :construction: submit members menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,58 @@ import {useDispatch, useSelector} from "./store/reduxHook";
 import {useInput} from "./cutomHook";
 import {initialMemberCount} from "./store/slice/memberSlice";
 import InputName from "./InputName";
+import {chooseRandomCafe} from "./store/slice/cafeSlice";
 
 function App() {
     const dispatch = useDispatch();
     const member = useSelector(state => state.member);
+    const cafe = useSelector(state => state.cafe);
     const [step, setStep] = useState(0);
     const [memberCount, onChangeMemberCount] = useInput(0);
 
     const settingSlot = useCallback(() => {
-        dispatch(initialMemberCount(Number(memberCount)))
-    },[memberCount]);
+        if(memberCount > 0) {
+            dispatch(initialMemberCount(Number(memberCount)));
+            setStep(1);
+        }
+    }, [memberCount]);
+
+    const getRandomCafe = useCallback(() => {
+        dispatch(chooseRandomCafe())
+        setStep(2)
+    }, []);
+
 
     useEffect(() => {
         console.log(member.member)
-    }, [member])
+    }, [member]);
 
     return (
         <div className="App">
             {step === 0 && (
                 <div>
+                    <p>Select your team members</p>
+                    <input type='number' value={memberCount} onChange={onChangeMemberCount}/>
+                    <button onClick={settingSlot}>Go</button>
+                </div>
+            )}
+            {step === 1 && (
+                <>
                     <div>
-                        <p>Select your team members</p>
-                        <input type='number' value={memberCount} onChange={onChangeMemberCount}/>
-                        <button onClick={settingSlot}>Go</button>
+                        <button onClick={getRandomCafe}>start</button>
                     </div>
-                    <div>
-                        {member.member.length > 0 && (
-                            member.member.map((mem, i) => (
-                                <InputName info={mem} key={i}/>
-                            ))
-                        )}
-                    </div>
+                </>
+            )}
+            {step === 2 && (
+                <div>
+                    <p>{cafe.cafe}</p>
+                    {member.member.length > 0 && (
+                        member.member.map((mem, i) => (
+                            <div key={i}>
+                                <InputName info={mem} />
+                            </div>
+                        ))
+                    )}
                 </div>
             )}
         </div>
--- src/InputName.tsx
+++ src/InputName.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import { useInput } from "./cutomHook";
 import {IMemberInfo} from "./store/slice/memberSlice";
+import {useSelector} from "./store/reduxHook";
+import {cafeList, ICafeList, ICafeMenuInfo} from "./cafe_info";
 
 interface IProps {
     info: IMemberInfo
@@ -8,13 +10,55 @@ interface IProps {
 
 const InputName = (props: IProps) => {
     const [name, changeName, setName] = useInput(props.info.name);
+    const cafe = useSelector(state => state.cafe);
+    const [cafeInfo, setCafeInfo] = useState<ICafeList>();
+    const [category, setCategory] = useState('');
+    const [menus, setMenus] = useState<ICafeMenuInfo[]>([]);
+    const [menu, setMenu] = useState<ICafeMenuInfo>();
+
+    useEffect(() => {
+        setCafeInfo(cafeList.find(item => item.name === cafe.cafe))
+    },[cafe.cafe]);
+
+    useEffect(() => {
+        if(cafeInfo && cafeInfo.name === cafe.cafe && category !== '') {
+            setMenus(cafeInfo.cafe_menu.find(item => item.category === category)!.menus);
+        }
+    },[cafe, cafeInfo, category])
+
+    const selectCategory = useCallback((e) => {
+        e.preventDefault();
+        setCategory(e.target.value)
+    },[])
+
+    const selectMenu = useCallback((e) => {
+
+    },[])
 
     return (
         <>
             <input
                 value={name}
                 onChange={changeName}
+                placeholder='member name'
             />
+            <select value={category} onChange={selectCategory}>
+                <option value=''>select category</option>
+                {cafeInfo?.cafe_menu.map((cate) => (
+                    <option value={cate.category} key={cate.category}>{cate.category}</option>
+                ))}
+            </select>
+            {
+                category !== '' && menus.length > 0 && (
+                    <>
+                        <select value={menu?.name} onChange={selectMenu}>
+                            {menus.map((item) => (
+                                <option value={item.name} key={item.name}>{item.name}</option>
+                            ))}
+                        </select>
+                    </>
+                )
+            }
         </>
     )
 }
